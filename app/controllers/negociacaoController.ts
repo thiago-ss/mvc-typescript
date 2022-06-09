f39461b3a1b6992b4264d6d513cce564381f5f57
Thiago <thiago2508ss@gmail.com>
2022-06-09 11:55:15 -0300

    Activating strictNullChecks and fixing errors

--- app/controllers/negociacaoController.ts
+++ app/controllers/negociacaoController.ts
@@ -11,13 +11,11 @@ export class NegociacaoController {
 	private negociacoes = new Negociacoes();
 	private negociacoesView = new NegociacoesView("#negociacoesView");
 	private mensagemView = new MensagemView("#mensagemView");
-	private DOMINGO = 0;
-	private SABADO = 6;
 
 	constructor() {
-		this.inputData = document.querySelector("#data");
-		this.inputQuantidade = document.querySelector("#quantidade");
-		this.inputValor = document.querySelector("#valor");
+		this.inputData = document.querySelector("#data") as HTMLInputElement;
+		this.inputQuantidade = document.querySelector("#quantidade") as HTMLInputElement;
+		this.inputValor = document.querySelector("#valor") as HTMLInputElement;
 		this.negociacoesView.update(this.negociacoes);
 	}
 
